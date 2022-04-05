38ddd42bbe9c995d4a07612de77406182be0f40c
LucaFedericoMarty <101400526+LucaFedericoMarty@users.noreply.github.com>
2022-04-05 14:23:01 -0300

    Ej 9 cambiado

--- Assets/Ej9S.cs
+++ Assets/Ej9S.cs
@@ -10,9 +10,9 @@ public class Ej9S : MonoBehaviour
     // Ingresar por Inspector el ingreso del capital aportado por cada una de las personas y luego  mostrar lo pedido en el siguiente formato:  
     // Nombre: capital aportado: $ … , Porcentaje del capital: %.... , Monto total aportado: $ …. 
 
-    public int cap1;
-    public int cap2;
-    public int cap3;
+    public float cap1;
+    public float cap2;
+    public float cap3;
     public string nom1;
     public string nom2;
     public string nom3;
@@ -20,25 +20,25 @@ public class Ej9S : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        int captot = cap1 + cap2 + cap3;
+        float captot = cap1 + cap2 + cap3;
         float porc1 = (cap1 / captot) * 100;
         float porc2 = (cap2 / captot) * 100;
         float porc3 = (cap3 / captot) * 100;
 
         Debug.Log("Nombre: " + nom1);
-        Debug.Log("Capital aportado: " + cap1);
-        Debug.Log("Porcentaje del capital: " + porc1);
-        Debug.Log("Monto total aportado: " + captot);
+        Debug.Log("Capital aportado: $" + cap1);
+        Debug.Log("Porcentaje del capital: %" + porc1);
+        Debug.Log("Monto total aportado: $" + captot);
 
         Debug.Log("Nombre: " + nom2);
-        Debug.Log("Capital aportado: " + cap2);
-        Debug.Log("Porcentaje del capital: " + porc2);
-        Debug.Log("Monto total aportado: " + captot);
+        Debug.Log("Capital aportado: $" + cap2);
+        Debug.Log("Porcentaje del capital: %" + porc2);
+        Debug.Log("Monto total aportado: $" + captot);
 
         Debug.Log("Nombre: " + nom3);
-        Debug.Log("Capital aportado: " + cap3);
-        Debug.Log("Porcentaje del capital: " + porc3);
-        Debug.Log("Monto total aportado: " + captot);
+        Debug.Log("Capital aportado: $" + cap3);
+        Debug.Log("Porcentaje del capital: %" + porc3);
+        Debug.Log("Monto total aportado: $" + captot);
 
         // Update is called once per frame
         void Update()
