276580f847694d1a8f6cc14b2bda82f5f0ddcf12
LucaFedericoMarty <101400526+LucaFedericoMarty@users.noreply.github.com>
2022-04-05 20:01:31 -0300

    Ejs 13 , 14  y 15

---
+++ Assets/Ej14S.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ej14S : MonoBehaviour
+{
+    //Construir un programa que simule el funcionamiento de una calculadora que puede  realizar las cuatro operaciones aritméticas básicas(suma, resta, producto y división) con valores numéricos enteros.
+    //Se debe especificar la operación que se desea realizar ingresando una letra por Inspector (s para la suma, r para la resta, p para el producto y d para la división) y luego, deberán ingresarse dos números enteros para así realizar la operación.Informar el resultado por pantalla.Nota: 
+    //Se recomienda el empleo de una sentencia switch.
+
+    public int valor1;
+    public int valor2;
+    public string letraparaoperacion;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        int resultado;
+
+        switch (letraparaoperacion)
+        {
+            case "s":
+                resultado = valor1 + valor2;
+                    Debug.Log("El resultado de esta suma es: " + resultado);
+                break;
+
+            case "r":
+                resultado = valor1 - valor2;
+                    Debug.Log("El resultado de esta resta es: " + resultado);
+                break;
+
+            case "m":
+                resultado = valor1 * valor2;
+                    Debug.Log("El resultado de esta multiplicacion es: " + resultado);
+                break;
+
+            case "d":
+                resultado = valor1 / valor2;
+                    Debug.Log("El resultado de esta division es: " + resultado);
+                break;
+
+            default:
+                Debug.Log("La letra ingresada no es valida");
+                break;
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/Ej15S.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ej15S : MonoBehaviour
+{
+    // Existen dos reglas que identifican dos conjuntos de valores: 
+    //- El número es de un solo dígito.
+    //- El número es impar.
+    //A partir de estas reglas, creá un algoritmo que asigne a las variables booleanas estaEnA, estaEnB, estaEnAmbos y noEstaEnNinguno el valor verdadero o falso, según corresponda, para indicar si el valor ingresado pertenece al primer conjunto, al segundo, a ambos o a  ninguno, respectivamente.
+    //Definí un lote de prueba de varios números y probá el algoritmo, escribiendo los resultados.
+
+    public int numero;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        bool estaEnA;
+        bool estaEnB;
+        bool estaEnAmbos;
+        bool noEstaEnNinguno;
+
+        estaEnA = numero % 2 > 0;
+        estaEnB = numero < 10;
+        estaEnAmbos = (numero % 2 > 0) && (numero < 10);
+        noEstaEnNinguno = numero % 2 < 0 && numero > 10;
+
+        if (estaEnA)
+        {
+            Debug.Log("El numero es impar");
+        }
+
+        else if (estaEnB)
+        {
+            Debug.Log("El numero es de una cifra");
+        }
+
+        else if (estaEnAmbos)
+        {
+            Debug.Log("El numero es tanto impar como de una cifra");
+        }
+
+        else if (noEstaEnNinguno)
+        {
+            Debug.Log("El numero no es ni impar ni de una cifra");
+        }
+
+
+
+
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
